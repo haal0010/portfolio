4429cd0f77c31c80cbb176e61ca67fc7f9069cc5
Haal0010 <haal0010@stud.kea.dk>
2025-01-07 04:13:03 +0100

    push section fix

--- menu.js
+++ menu.js
@@ -2,12 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector("nav");
   const menu = document.querySelector(".menu");
+  const sections = document.querySelectorAll(".about-me, .portfolio, .icons, .skills-line");
 
   burger.addEventListener("click", burgerClick);
 
   function burgerClick() {
     burger.classList.toggle("active");
     nav.classList.toggle("active");
+
+    // Move sections when the menu is active
+    sections.forEach((section) => {
+      section.classList.toggle("active");
+    });
   }
 
   menu.addEventListener("click", menuClick);
@@ -15,5 +21,9 @@ document.addEventListener("DOMContentLoaded", function () {
   function menuClick() {
     burger.classList.remove("active");
     nav.classList.remove("active");
+    // Reset section movement
+    sections.forEach((section) => {
+      section.classList.remove("active");
+    });
   }
 });
